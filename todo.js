56ede65f4a1966d3a9189d5090172649f9d18458
Awais <awaisraza@gmail.com>
2022-07-23 07:50:55 -0700

    fuly complete todo list

--- todo.js
+++ todo.js
@@ -1,9 +1,15 @@
 const todoadd = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+
+
+
 const generateTemplate = todo =>{
     const html = 
     `<li class="list-group-item d-flex justify-content-between align-item-center">
     <span>${todo}</span>
+    <i class="fa-solid fa-trash delete"></i>
     
     </li>
     `;
@@ -21,3 +27,30 @@ todoadd.addEventListener('submit', e =>{
 
     }
 })
+
+list.addEventListener('click', e=>{
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos = (term) =>{
+Array.from(list.children)
+.filter((todo)=> !todo.textContent.toLowerCase().includes(term))
+.forEach((todo)=> todo.classList.add('flitered'))
+
+.filter((todo)=> todo.textContent.toLowerCase().includes(term))
+.forEach((todo)=> todo.classList.remove('flitered')) 
+
+
+}
+
+
+
+search.addEventListener('keyup', e =>{
+    e.preventDefault();
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term)
+});
+
+
